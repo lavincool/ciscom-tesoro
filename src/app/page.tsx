2c1b4232ad3bc13f333a8027a58e2adbeb10ec0e
Angel Lavin <lavindev@proton.me>
2024-10-17 04:47:04 -0500

    Improve main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Tesoro } from "./components/Tesoso";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 items-center m-auto">
+    <div className="grid grid-rows-[10px_1fr_10x] items-center justify-items-center min-h-screen p-8 pt-0 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-2 items-center m-auto">
         <Image
           src="https://devcloud.raza.cool/ciscom/tesoro/logo_png.png"
           alt="CISCOM logo"
@@ -13,14 +13,15 @@ export default function Home() {
           className="m-auto"
           priority
         />
+        <h1 className="center text-center text-lg mb-4">Capítulo de Ingenieria en Sistemas Computacionales <p className="font-semibold text-yellow-400 mt-6">~ Busqueda del tesoro ~</p></h1>
         <ol className="list-inside list-decimal text-lg text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
+          <li className="mb-4">
             Sigue las pistas y obten los códigos. 
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               (Guardalos)
             </code>
           </li>
-          <li className="mb-2">Dale click donde dice <b>abrir cofre</b> e ingresa los dós códigos para obtener el tesoro</li>
+          <li className="mb-4">Dale click donde dice <b>abrir cofre</b> e ingresa los dós códigos para obtener el tesoro</li>
         </ol>
 
         <Tesoro/>
